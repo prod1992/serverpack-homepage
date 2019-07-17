8f5cf1ee30b3b644e86b63976d76cbe13bc7fe1c
Astx1999 <astxofficial@gmail.com>
2019-07-17 18:21:31 +0400

    17.07.19

--- src/js/index.js
+++ src/js/index.js
@@ -2,27 +2,53 @@ import "bootstrap";
 import Flickity from "flickity";
 import "../scss/index.scss";
 import './typed-text';
-
+import AOS from 'aos';
+// import isotope from "isotope-layout";
+import "./isotope";
 $("#alert").click(() => {
   alert("jQuery works!");
 });
 
 // Your jQuery code 
-$(function(){
-  $(window).scroll(function(){
-  
-    if($(this).scrollTop()>=30){
-        $("header").css("background-color", "#1B1F3B")
-    }else{
-        $("header").css("background-color", "transparent")
+$(function () {
+  $(document).ready(function () {
+    if ($(window).scrollTop() >= 30) {
+      $("header").css("background-color", "#1B1F3B")
+    } else {
+      $("header").css("background-color", "transparent")
+    }
+  })
+
+
+  $(window).scroll(function () {
+
+    if ($(this).scrollTop() >= 30) {
+      $("header").css("background-color", "#1B1F3B")
+    } else {
+      $("header").css("background-color", "transparent")
     }
   });
-  var flkty = new Flickity( '.carousel', {
-    wrapAround: true, 
+  var flkty = new Flickity('.carousel', {
+    wrapAround: true,
     pageDots: true,
     autoPlay: true,
     imagesLoaded: true
   });
+  AOS.init();
+  
+  $('.dropdown-menu a.dropdown-item').on('click', function(e) {
+	  if (!$(this).next().hasClass('show')) {
+		$(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+	  }
+	  var $subMenu = $(this).next(".dropdown-menu");
+	  $subMenu.toggleClass('show');
+
+	  $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+		$('.dropdown-submenu .show').removeClass("show");
+	  });
+
+	  return false;
+	});
 });
 
 
